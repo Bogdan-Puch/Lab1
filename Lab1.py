7730792d50f8b13ab5b8f3180a7e420f217d50b6
Bogdan-Puch <bodyprofe@gmail.com>
2025-09-10 10:35:11 +0300

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,20 @@
+age = 26
+height = 199.9
+name = ' Kai '
+city = ' London'
+money = True
+baby = False
+age_people = [ 20 , 25 , 30]
+coordinates = ( 20 , 50 )
+kilometer = { 10 , 50 , 100}
+person = { 'name' : 'Kai' , 'age' : 20 , 'height' : 199.9 , 'city' : 'London' }
+print(name , type(name))
+print(age , type(age))
+print(height , type(height))
+print(city , type(city))
+print(money , type(money))
+print(baby ,type(baby))
+print(age_people , type(age_people))
+print(coordinates , type(coordinates))
+print(kilometer , type(kilometer))
+print(person , type(person))
