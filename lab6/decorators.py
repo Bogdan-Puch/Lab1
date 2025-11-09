ce8cf8a6de616976cf350d5f7b931cbab46077a7
Bogdan-Puch <bodyprofe@gmail.com>
2025-11-09 21:53:50 +0200

    Delete lab6 directory

--- lab6/decorators.py
+++
@@ -1,8 +0,0 @@
-import time
-
-def delay(func):
-    def wrapper(*args, **kwargs):
-        print("Зачекай 5 секунд...")
-        time.sleep(5)   # затримка
-        return func(*args, **kwargs)  # виклик оригінальної функції
-    return wrapper
